6c66a3a399dec8e454c8e61129a6afb66886ee64
Leo Kosta <kostaleonard@gmail.com>
2023-03-31 07:19:19 -0400

    Added basic argument parser
    Signed-off-by: Leo Kosta <kostaleonard@gmail.com>

--- backseat_driver/backseat_driver.py
+++ backseat_driver/backseat_driver.py
@@ -7,15 +7,22 @@ import sys
 def get_args(args: list[str]) -> Namespace:
     """Returns a Namespace containing the command line arguments.
 
-    :param args: The raw command line arguments; sys.argv.
+    :param args: The raw command line arguments. As in argparse, this function
+        expects the value to be sys.argv[1:].
+    :return: The argparse.Namespace containing the processed command line
+        arguments.
     """
-    # TODO
+    parser = ArgumentParser(
+        description="Requests a code review from a large language model."
+    )
+    # TODO add args, add arg tests, then add CI
+    # TODO see README for expected args
+    return parser.parse_args(args=args)
 
 
 def main() -> None:
     """Runs a code review on the user-specified directory."""
-    args = get_args(sys.argv)
-    # TODO
+    _ = get_args(sys.argv[1:])
 
 
 if __name__ == "__main__":

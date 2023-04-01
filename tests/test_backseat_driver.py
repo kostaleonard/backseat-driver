a8c550db1dc65f7696907fbe4bc3a2aaf1bde9c5
Leo Kosta <kostaleonard@gmail.com>
2023-04-01 09:20:18 -0400

    Added get_args tests
    Signed-off-by: Leo Kosta <kostaleonard@gmail.com>

--- tests/test_backseat_driver.py
+++ tests/test_backseat_driver.py
@@ -1,3 +1,29 @@
 """Tests backseat_driver.py."""
 
-# TODO
+import pytest
+from backseat_driver import backseat_driver
+
+
+def test_get_args_returns_namespace_containing_args() -> None:
+    """Tests that get_args returns a namespace containing the command line
+    arguments."""
+    args = backseat_driver.get_args(
+        ["--openai_key=123", "--fail_under=B", "--filter_files_by_suffix=.py"]
+    )
+    assert args.source_directory == "."
+    assert args.filter_files_by_suffix == ".py"
+    assert args.fail_under == "B"
+    assert args.openai_key == "123"
+
+
+def test_get_args_raises_error_when_fail_under_invalid() -> None:
+    """Tests that get_args raises an error when fail_under is not a letter
+    grade."""
+    with pytest.raises(SystemExit):
+        _ = backseat_driver.get_args(["--openai_key=123", "--fail_under=Q"])
+
+
+def test_get_args_raises_error_when_openai_key_unspecified() -> None:
+    """Tests that get_args raises an error when openai_key is unspecified."""
+    with pytest.raises(SystemExit):
+        _ = backseat_driver.get_args([])
